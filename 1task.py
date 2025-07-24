0e791ffb5cac968013af0f7ef224c5c81b8c3226
White_Ninja_07 <109147436+BILLOW07@users.noreply.github.com>
2025-07-24 16:10:37 +0300

    Delete 1task.py

--- 1task.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Введите число a: "))
-b = int(input("Введите число b: "))
-
-total = sum(range(a, b + 1)) if a <= b else sum(range(b, a + 1))
-print(f"Сумма чисел от {a} до {b} включительно: {total}")
